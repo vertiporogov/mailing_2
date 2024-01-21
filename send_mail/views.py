c1ec8543113235848430a533da7cf753c7d3eea6
alexander <vertiporogov.alexandr@mail.ru>
2024-01-21 21:46:41 +0300

    vse na segodni

--- send_mail/views.py
+++ send_mail/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from send_mail.models import Client
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ('name', 'email', 'comment',)
+
+    def form_valid(self, form):
+        pass
