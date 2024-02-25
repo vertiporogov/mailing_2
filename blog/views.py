f4da4bcd4b125c0f4c0d66158b85794334ba3c42
alexander <vertiporogov.alexandr@mail.ru>
2024-02-25 17:04:24 +0300

    course project fixed

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,6 @@ class MailingBlogDetailView(DetailView):
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
         self.object.views_count += 1
-        self.object.save()
+        self.object.save(update_fields=['views_count'])
 
         return self.object
